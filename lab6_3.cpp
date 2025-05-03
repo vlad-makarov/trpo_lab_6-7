123283b508a7447d33bdb8acb1a4214d479b8963
vlad-makarov <vladisden2004@mail.ru>
2025-05-03 13:53:16 +0400

    Добавлен базовый класс Product и конкретные продукты

---
+++ lab6_3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+// Базовый класс продукта
+struct Product {
+    string name;
+    double weight;
+    virtual ~Product() {}
+    virtual void accept(class NutritionVisitor& visitor) = 0;
+};
+
+// Конкретные продукты
+struct Mushroom : Product {
+    Mushroom(double w) { name = "Шампиньоны"; weight = w; }
+    void accept(NutritionVisitor& visitor) override;
+};
+
+struct Shrimp : Product {
+    Shrimp(double w) { name = "Креветки"; weight = w; }
+    void accept(NutritionVisitor& visitor) override;
+};
+
+struct SourCream : Product {
+    SourCream(double w) { name = "Сметана"; weight = w; }
+    void accept(NutritionVisitor& visitor) override;
+};
+
+struct Cheese : Product {
+    Cheese(double w) { name = "Сыр"; weight = w; }
+    void accept(NutritionVisitor& visitor) override;
+};
+
+struct Dill : Product {
+    Dill(double w) { name = "Укроп и пряности"; weight = w; }
+    void accept(NutritionVisitor& visitor) override;
+};
