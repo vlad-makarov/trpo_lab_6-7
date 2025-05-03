19713f1406431cde105953b3ca3709bdb5dcd570
vlad-makarov <vladisden2004@mail.ru>
2025-05-03 13:32:23 +0400

    Добавлены базовые классы иерархии Expression

---
+++ lab6_12.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+struct Transformer;
+
+// Expression (базовый класс)
+struct Expression {
+    virtual ~Expression() {}
+    virtual double evaluate() const = 0;
+    virtual Expression* transform(Transformer* tr) const = 0;
+    virtual void print(std::ostream& out) const = 0;
+};
+
+// Number
+struct Number : Expression {
+    explicit Number(double value) : value_(value) {}
+    double value() const { return value_; }
+
+    double evaluate() const override { return value_; }
+
+    Expression* transform(Transformer* tr) const override;
+    void print(std::ostream& out) const override { out << value_; }
+
+private:
+    double value_;
+};
+
+// Variable
+struct Variable : Expression {
+    explicit Variable(std::string const& name) : name_(name) {}
+    std::string const& name() const { return name_; }
+
+    double evaluate() const override { return 0.0; }
+
+    Expression* transform(Transformer* tr) const override;
+    void print(std::ostream& out) const override { out << name_; }
+
+private:
+    std::string const name_;
+};
